dbd9b63e59d59300069d769137e7c61a525aa0bf
Donteatpineappleonpizza <winittra_saengsroy@cmu.ac.th>
2022-09-15 14:49:28 +0700

    fn

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -5,9 +5,47 @@ export default function Comment(props) {
   return (
     <div>
       {/* Entire Comment div */}
-      <div></div>
+      <div>
+        <div className="d-flex gap-2 my-2">
+          <img
+            src={props.profileImg}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+              {props.username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{props.content}</span>
+            <div
+              className={
+                props.likeNum > 0 ? "d-flex align-items-center gap-1" : "d-none"
+              }
+            >
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{props.likeNum}</span>
+            </div>
+          </div>
+        </div>
 
-      {/*render Reply here... */}
+        {props.replies.map((reply, i) => {
+          return (
+            <Reply
+              key={i}
+              username={reply.username}
+              content={reply.replyText}
+              profileImg={reply.userImagePath}
+              likeNum={reply.likeNum}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- components/PostOwner.jsx
+++ components/PostOwner.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function PostOwner(props) {
-  return <div></div>;
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <img
+          src="/profileImages/IMG_1980.jpg"
+          width="48"
+          height="48"
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+        />
+        <span className="fw-semibold fs-5 text-white">
+          Winittra Saengsroy 640612097
+        </span>
+      </div>
+      <span className="text-white">P'Arm very hansome #261207</span>
+      <div className="d-flex align-items-center gap-1">
+        <img src="/like.svg" width={20}></img>
+        <span style={{ color: "#B0B3B8" }}>1 คน</span>
+      </div>
+      <hr className="m-0 border" />
+    </div>
+  );
 }
